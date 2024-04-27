6d1d210c53ea95a7e82fa8e3082d2a4211163b95
Mahmoud Ali <eng.mahmoudali94@gmail.com>
2024-04-28 01:49:37 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Method to determine if all boxes can be opened
+"""
+
+def canUnlockAll(boxes):
+    """
+    Check if boxes can be unlocked
+    """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
