6aa135bf11aae760fc6d3daf578ae0b68eb4f5ec
Mahmoud Ali <eng.mahmoudali94@gmail.com>
2024-06-09 23:05:17 +0300

    make changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Change making module.
+"""
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given
+    the given total
+    """
+
+    if total <= 0:
+        return 0
+
+    current_total = 0
+    coin_used = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = (total-current_total)//coin
+        current_total += r*coin
+        coin_used += r
+        if current_total == total:
+            return coin_used
+    return -1
